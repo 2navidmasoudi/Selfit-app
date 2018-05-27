3a14fb620bbe1994d1d020c987e1279252c92bf8
Navid Masoudi <2navidmasoudi@gmail.com>
2018-05-28 01:33:30 +0430

    fix: disable codepush and app start from splash

--- src/App.js
+++ src/App.js
@@ -87,7 +87,6 @@ const onBeforeLift = async () => {
   );
 };
 
-@codePush
 export default class App extends Component {
   componentDidMount() {
     Linking.getInitialURL().then((url) => {
@@ -193,9 +192,9 @@ export default class App extends Component {
           >
             <RouterWithRedux hideNavBar>
               <Scene key="rootMain" hideNavbar>
-                <Scene key="splash"  component={Splash} hideNavBar />
+                <Scene key="splash" initial component={Splash} hideNavBar />
                 <Scene key="sign" component={Sign} hideNavBar />
-                <Scene key="root" initial hideNavBar>
+                <Scene key="root" hideNavBar>
                   <Drawer
                     key="drawer"
                     drawerPosition="right"
