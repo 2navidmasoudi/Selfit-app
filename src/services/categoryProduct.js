3c23b1b264c50ee8cea55e21112d8e6ce563d7f8
Navid Masoudi <2navidmasoudi@gmail.com>
2018-08-10 23:16:44 +0430

    test1

--- src/services/categoryProduct.js
+++ src/services/categoryProduct.js
@@ -1,7 +1,7 @@
 import {CategoryProduct, GET, headers, SAPI, Selfit } from './type';
 
 export const getAllCategoryProduct = async (token, tokenapi, max, min, sort) => {
-  const response = await fetch(`${SAPI}${CategoryProduct}GetAll?token=${token}&tokenapi=${tokenapi}&max=${max}&min=${min}&sort=${sort}`, {
+  const response = await fetch(`${SAPI}${CategoryProduct}GetAll?t.token=${token}&t.tokenapi=${tokenapi}&p.max=${max}&p.min=${min}&p.sort=${sort}`, {
     method: GET,
     headers
   });
